d8455583989c5f03f183c0e973cf310816f276c2
codemzy <codemzy@gmail.com>
2018-09-03 17:00:16 +0000

    Added availability pill and made title bold

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Layout from '../components/layout'
 export default () => (
   <Layout title='The Name'>
     <div className="container">
-      <h1 className="display-1">Name Surname</h1>
-      <p><span className="badge badge-dark">HTML</span> <span className="badge badge-dark">JavaScript</span> <span className="badge badge-dark">ReactJS</span></p>
+      <h1 className="display-1 font-weight-bold">Name Surname</h1>
+      <div className="d-flex justify-content-between">
+        <div><span className="badge badge-dark">HTML</span> <span className="badge badge-dark">JavaScript</span> <span className="badge badge-dark">ReactJS</span></div>
+        <div><span className="badge badge-pill badge-success">Available Now</span></div>
+      </div>
       <hr/>
       <section>
         <h2>Profile</h2>
