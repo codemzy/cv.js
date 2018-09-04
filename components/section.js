084d20750e03049c2f8cfa867c02ee525c861321
codemzy <codemzy@gmail.com>
2018-09-04 13:47:29 +0000

    Updated section display

--- components/section.js
+++ components/section.js
@@ -1,6 +1,6 @@
 export default ({ children, title = '' }) => (
-  <section>
-    <h2>{title}</h2>
+  <section className="mt-5 pb-4">
+    <h2 className="font-weight-bold text-uppercase pb-3">{title}</h2>
     { children }
   </section>
 )
