3f64780cf482f95057e2af2a54a12502f68d35f1
codemzy <codemzy@gmail.com>
2018-09-05 12:58:16 +0000

    Updated readme and wired up create CV button

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default ({ children, title = 'This is an example CV' }) => (
     <header>
       <nav className="navbar navbar-light bg-white flex-row-reverse">
         <form className="form-inline">
-          <button className="btn btn-outline-primary" type="button">Create CV</button>
+          <a href="https://codemzy.github.io/cv.js" className="btn btn-outline-primary" type="button">Create CV</a>
         </form>
       </nav>
     </header>
