71186e78bd63a96bfdcea22ca15fbd1eb91b734a
codemzy <codemzy@gmail.com>
2018-09-05 12:47:18 +0000

    Static export set up for next.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// for static deployment
+module.exports = {
+  exportPathMap: function(defaultPathMap) {
+    return {
+      '/': { page: '/' }
+    }
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/cv.js' : '',
+}
